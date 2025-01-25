3cfa16fc996e3d3c2ffd6acae268859a22f888d5
Vinay Joshi <vinay01joshi@gmail.com>
2025-01-25 08:42:31 +0530

    checking for deployed version

--- src/pages/wifiSetup.js
+++ src/pages/wifiSetup.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import { View, Text, TextInput, Button } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import axios from "axios";
+import { Divider } from "@ui-kitten/components";
 
 export default function WifiSetup() {
   const [networks, setNetworks] = useState([]);
   const [selectedNetwork, setSelectedNetwork] = useState("");
   const [password, setPassword] = useState("");
+  const [response, setResponse] = useState("");
   useEffect(() => {
     axios
       .get("http://192.168.4.1/networks")
@@ -20,6 +22,13 @@ export default function WifiSetup() {
       .post("http://192.168.4.1/wifi", data)
       .then((res) => console.log(res.data))
       .catch((err) => console.error("Error sending credentials:", err));
+  };
+
+  const refreshNetworks = () => {
+    axios
+      .get("http://192.168.4.1/networks")
+      .then((response) => setResponse(response.data))
+      .catch((err) => setResponse(err));
   };
 
   return (
@@ -43,7 +52,10 @@ export default function WifiSetup() {
         style={{ borderWidth: 1, marginVertical: 10, padding: 10 }}
         onChangeText={setPassword}
       />
+      <TextInput>{JSON.stringify(response)} </TextInput>
       <Button title="Connect to Wi-Fi" onPress={sendCredentials} />
+      <Divider />
+      <Button title="Referesh" onPress={refreshNetworks} />
     </View>
   );
 }
