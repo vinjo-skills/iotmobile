2d1f305debb29699cd52bd07821b3a0c39585adc
Vinay Joshi <vinay01joshi@gmail.com>
2025-01-25 20:41:22 +0530

    adding more logging on app

--- src/pages/wifiSetup.js
+++ src/pages/wifiSetup.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import axios from "axios";
 import { Divider } from "@ui-kitten/components";
@@ -8,12 +15,22 @@ export default function WifiSetup() {
   const [networks, setNetworks] = useState([]);
   const [selectedNetwork, setSelectedNetwork] = useState("");
   const [password, setPassword] = useState("");
-  const [response, setResponse] = useState("");
+  const [response, setResponse] = useState("click button for loading...");
+  const [fetchresponse, setFetchResponse] = useState(
+    "click button for loading..."
+  );
+  const [loading, setloading] = useState(false);
   useEffect(() => {
     axios
       .get("http://192.168.4.1/networks")
-      .then((response) => setNetworks(response.data))
-      .catch((err) => alert("please connect to vinjo network"));
+      .then((response) => {
+        setNetworks(response.data);
+        setloading(true);
+      })
+      .catch((err) => {
+        setloading(true);
+        alert("please connect to vinjo network");
+      });
   }, []);
 
   const sendCredentials = () => {
@@ -25,37 +42,78 @@ export default function WifiSetup() {
   };
 
   const refreshNetworks = () => {
+    setResponse("Loading...");
     axios
       .get("http://192.168.4.1/networks")
-      .then((response) => setResponse(response.data))
-      .catch((err) => setResponse(err));
+      .then((response) => setResponse(JSON.stringify(response.data)))
+      .catch((err) => setResponse(JSON.stringify(err)));
+  };
+
+  const refreshByFetch = () => {
+    setFetchResponse("Loading...");
+    fetch("http://192.168.4.1/networks")
+      .then((response) => response.json())
+      .then((data) => {
+        setFetchResponse(JSON.stringify(data));
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+        setFetchResponse(JSON.stringify(error));
+      });
   };
 
   return (
-    <View style={{ padding: 20 }}>
-      <Text>Select a Wi-Fi Network</Text>
-      <Picker
-        selectedValue={selectedNetwork}
-        onValueChange={(value) => setSelectedNetwork(value)}
-      >
-        {networks.map((network, idx) => (
-          <Picker.Item
-            label={`${network.ssid} (Signal: ${network.rssi})`}
-            value={network.ssid}
-            key={idx}
-          />
-        ))}
-      </Picker>
-      <TextInput
-        placeholder="Enter Wi-Fi Password"
-        secureTextEntry
-        style={{ borderWidth: 1, marginVertical: 10, padding: 10 }}
-        onChangeText={setPassword}
-      />
-      <TextInput>{JSON.stringify(response)} </TextInput>
-      <Button title="Connect to Wi-Fi" onPress={sendCredentials} />
-      <Divider />
-      <Button title="Referesh" onPress={refreshNetworks} />
-    </View>
+    <ScrollView>
+      <View style={{ padding: 20, backgroundColor: "white" }}>
+        <Text>Select a Wi-Fi Network</Text>
+        <Picker
+          selectedValue={selectedNetwork}
+          onValueChange={(value) => setSelectedNetwork(value)}
+        >
+          {networks.map((network, idx) => (
+            <Picker.Item
+              label={`${network.ssid} (Signal: ${network.rssi})`}
+              value={network.ssid}
+              key={idx}
+            />
+          ))}
+        </Picker>
+        <TextInput
+          placeholder="Enter Wi-Fi Password"
+          secureTextEntry
+          style={{ borderWidth: 1, marginVertical: 10, padding: 10 }}
+          onChangeText={setPassword}
+        />
+        <Button title="Connect to Wi-Fi" onPress={sendCredentials} />
+        <Divider />
+
+        <View
+          style={{
+            backgroundColor: "lightgray",
+            padding: 10,
+            marginVertical: 10,
+            height: 200,
+          }}
+        >
+          <Text style={{ height: 150 }}>{response} </Text>
+          <Button title="Referesh" onPress={refreshNetworks} />
+        </View>
+
+        <Divider />
+
+        <View
+          style={{
+            backgroundColor: "lightgray",
+            padding: 10,
+            marginVertical: 10,
+            height: 200,
+          }}
+        >
+          <Text style={{ height: 150 }}>{fetchresponse}</Text>
+          <Button title="Referesh By Fetch" onPress={refreshByFetch} />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
